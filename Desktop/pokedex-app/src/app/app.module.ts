578cf0deb634d9d943cc1354d1dbad7f99e588cb
mehdimoad <mehdi.moad@estiam.com>
2021-05-09 18:21:50 +0200

    first commit

---
+++ Desktop/pokedex-app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'; 
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PokemonListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    Ng2SearchPipeModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
